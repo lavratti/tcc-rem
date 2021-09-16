34fcf9474a42a22f3d69cdfe0d4ef12f12e809b6
Lucas Lavratti <lucaslavratti98@gmail.com>
2021-09-15 21:42:53 -0300

    funcionalidades

--- src/nosso_back_end.py
+++ src/nosso_back_end.py
@@ -8,14 +8,19 @@ import numpy as np
 
 
 def gerar_espectograma(amostra):
-    print("gerando espc")
+    print("Gerando espec. para {}".format(amostra))
     matplotlib.use('Agg') # No pictures displayed
     sig, fs = librosa.load(amostra)
     save_path = os.path.join('temp', '{}.jpg'.format(os.path.split(amostra)[1].split('.')[0]))
-    print(save_path)
     pylab.axis('off') # no axis
     pylab.axes([0., 0., 1., 1.], frameon=False, xticks=[], yticks=[]) # Remove the white edge
     S = librosa.feature.melspectrogram(y=sig, sr=fs)
     librosa.display.specshow(librosa.power_to_db(S, ref=np.max))
     pylab.savefig(save_path, bbox_inches=None, pad_inches=0)
     pylab.close()
+    print("Espec. OK")
+
+
+def processar_lista(lista):
+    for amostra in lista:
+        gerar_espectograma(amostra)

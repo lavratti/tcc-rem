00e012eb1302e3d52be9384bbc617d400b6612de
RafaeloXIII <37121770+RafaeloXIII@users.noreply.github.com>
2021-09-19 03:59:49 -0300

    Update TesteRafaelo.py

--- src/TesteRafaelo.py
+++ src/TesteRafaelo.py
@@ -16,3 +16,8 @@ S = librosa.feature.melspectrogram(y=sig, sr=fs)
 librosa.display.specshow(librosa.power_to_db(S, ref=np.max))
 pylab.savefig(save_path, bbox_inches=None, pad_inches=0)
 pylab.close()
+
+y, sr = librosa.load('teste.wav')
+onset_env = librosa.onset.onset_strength(y, sr=sr)
+tempo = librosa.beat.tempo(onset_envelope=onset_env, sr=sr)
+tempo

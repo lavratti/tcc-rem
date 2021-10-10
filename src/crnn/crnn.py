a9182a08fa5cd567130f9b3d3d23efe2ca1586b0
lucaslavratti98@gmail.com <BclnRvHn0IL3qM9>
2021-10-10 17:07:41 -0300

    CRNN mais proxima ao que esta no paper

--- src/crnn/crnn.py
+++ src/crnn/crnn.py
@@ -38,45 +38,25 @@ def plot_history(history):
     pylab.close()
 
 
-def build_model():
-    model = keras.Sequential([
-        # Input block
-        layers.BatchNormalization(name='bn0'),
-        layers.Reshape((128, 1940, 1)),
+def get_model():
 
-        # Conv block 1
-        layers.Convolution2D(64, 3, 3, name='conv1'),
-        layers.BatchNormalization(axis=3, name='bn1'),
-        layers.ELU(),
-        layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool1'),
-        layers.Dropout(0.1, name='dropout1'),
+    inputs = tf.keras.Input(shape=(128, 1940, 1))
 
-        # Conv block 2
-        layers.Convolution2D(128, 3, 3, name='conv2'),
-        layers.BatchNormalization(axis=3, name='bn2'),
-        layers.ELU(),
-        layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool2'),
-        layers.Dropout(0.1, name='dropout2'),
+    x = tf.keras.layers.MaxPooling2D(pool_size=(2, 28), strides=(2, 30))(inputs)
+    x = tf.keras.layers.Conv2D(64, (3, 3), activation='relu')(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(8))(x)
+    x = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(1))(x)
+    x = tf.keras.layers.Reshape(tuple([y for y in x.shape.as_list() if y != 1 and y is not None]))(x)
+    x = tf.keras.layers.GRU(8)(x)
 
-        # Conv block 3
-        layers.Convolution2D(128, 3, 3, name='conv3'),
-        layers.BatchNormalization(axis=3, name='bn3'),
-        layers.ELU(),
-        layers.Dropout(0.1, name='dropout3'),
+    outputs = tf.keras.layers.Dense(1)(x)
 
-        # GRU
-        layers.Reshape((17, 128)),
-        layers.GRU(32, return_sequences=True, name='gru1'),
-        layers.GRU(64, return_sequences=False, name='gru2'),
-
-        # Out
-        layers.Dropout(0.3, name='dropout4'),
-        layers.Dense(1, activation='sigmoid', name='output')
-    ])
+    model = tf.keras.Model(inputs=inputs, outputs=outputs)
 
     optimizer = tf.keras.optimizers.RMSprop(0.001)
-
     model.compile(loss='mse', optimizer=optimizer, metrics=['mae', 'mse'])
+
     return model
 
 if not os.path.isfile('dataset/melspectograms.pickle'):
@@ -88,7 +68,7 @@ print("Loading pickle")
 mean_arousals, mean_valences, train_dataset = mydataset.quick_load_500()
 print("Loaded pickle")
 
-model = build_model()
+model = get_model()
 model.build(np.shape(train_dataset))
 model.summary()
 
